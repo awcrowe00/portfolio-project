3d992282de40377a5d1354c4b1543b8667d9f474
awcrowe00 <76759525+awcrowe00@users.noreply.github.com>
2025-08-20 10:36:25 -0700

    attempting to get deployment via github pages working

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import svelte from "@astrojs/svelte";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://awcrowe00.github.io',
-  base: '/portfolio-project',
   integrations: [svelte()]
 });
